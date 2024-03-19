912b4408a2c7f0859f34f2d11a718148b0a1c8e5
Shivar Tuplah <128069501+ST10256115@users.noreply.github.com>
2024-03-19 22:31:43 +0200

    initial

---
+++ Data/LibraryApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LibraryApplication.Models;
+
+namespace LibraryApplication.Data
+{
+    public class LibraryApplicationContext : DbContext
+    {
+        public LibraryApplicationContext (DbContextOptions<LibraryApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LibraryApplication.Models.Books> Books { get; set; } = default!;
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryApplication.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string BorrowerEmail { get; set; }
+    }
+}
